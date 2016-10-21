2455b6b1c780d13ecee4a02d5238fd6f2c7fe980
cschlesser <cschlesser@oneall.com>
2016-10-21 16:14:34 +0200

    LoudVoice added

--- social-sharing-icons/admin/templates/oa-social-sharing-icons-admin-more.php
+++ social-sharing-icons/admin/templates/oa-social-sharing-icons-admin-more.php
@@ -6,19 +6,16 @@
  * @package    oa_social_sharing_icons
  */
 
-// Plugin URL
+// Plugin URLs
 $social_login_url = admin_url('plugin-install.php?s=oa_social_login&tab=search&type=term');
-
+$loudvoice_url = admin_url('plugin-install.php?s=loudvoice&tab=search&type=term');
 
 ?>
 <div class="wrap">
-	<div id="oneall">
+	<div id="oneall" class="oneall-more">
 		<h2>OneAll Social Sharing Icons <?php echo $this->version; ?></h2>
 	
 		<div class="oneall_box oneall_box_help">
-			<div class="oneall_box_title">
-				<?php _e ('Information', 'oa-social-sharing-icons'); ?>
-			</div>
 			<div class="oneall_box_contents">
 				<ul>
 					<li>
@@ -33,22 +30,23 @@ $social_login_url = admin_url('plugin-install.php?s=oa_social_login&tab=search&t
 				</ul>
 			</div>			
 		</div>		
+		<h1><?php _e ('Discover our other plugins!', 'oa-social-sharing-icons'); ?></h1>
 		<div class="oneall_box oneall_box_teaser">
-			<div class="oneall_box_title">
-				<?php _e ('OneAll Social Login', 'oa-social-sharing-icons'); ?>
-				<span class="oneall_pull_right">
-					<a href="<?php echo $social_login_url; ?>"><?php _e ('View Plugin Details', 'oa-social-sharing-icons'); ?></a>
-				</span>
+			<div class="oneall_box_img">
+				<a href="<?php echo $social_login_url; ?>"><img src="<?php echo plugin_dir_url( __FILE__ ) . '../img/teaser/social-login.png' ?>" alt="<?php _e ('Social Login', 'oa-social-sharing-icons') ?>" /></a>
+			</div>						
+			<div class="oneall_box_description">							
+				<?php _e ('Allow your visitors to comment, login and register with 30+ social networks like for example Twitter, Facebook, Pinterest, Instagram, Paypal, LinkedIn, OpenID, VKontakte or Google+. Easy to use and 100% FREE.', 'oa-social-sharing-icons'); ?>
+				<a href="<?php echo $social_login_url; ?>" class="oneall_btn oneall_btn_success"><?php _e ('More Info', 'oa-social-sharing-icons'); ?></a>
+			</div>		
+		</div>
+		<div class="oneall_box oneall_box_teaser">
+			<div class="oneall_box_img">
+				<a href="<?php echo $loudvoice_url; ?>"><img src="<?php echo plugin_dir_url( __FILE__ ) . '../img/teaser/loudvoice.png' ?>" alt="<?php _e ('LoudVoice Comment System', 'oa-social-sharing-icons') ?>" /></a>
 			</div>
-			<div class="oneall_box_contents">
-				<div class="oneall_box_section oneall_box_description">							
-					<?php _e ('Allow your visitors to comment, login and register with 30+ social networks like for example Twitter, Facebook, Pinterest, Instagram, Paypal, LinkedIn, OpenID, VKontakte or Google+. Easy to use and 100% FREE.', 'oa-social-sharing-icons'); ?>
-				</div>
-				<div class="oneall_box_section">
-					<a href="<?php echo $social_login_url; ?>">
-						<img class="theme_tease" src="<?php echo plugin_dir_url( __FILE__ ) . '../img/teaser/social-login.png' ?>" alt="<?php _e ('Social Login', 'oa-social-sharing-icons') ?>" />
-					</a>		
-				</div>
+			<div class="oneall_box_description">							
+				<?php _e ('LoudVoice replaces the basic WordPress comments by a powerful comment system that includes logging in with 30+ social networks, spam filters and more. Easy to use and 100% FREE. Existing comments can be imported!', 'oa-social-sharing-icons'); ?>
+				<a href="<?php echo $loudvoice_url; ?>" class="oneall_btn oneall_btn_success"><?php _e ('More Info', 'oa-social-sharing-icons'); ?></a>
 			</div>
 		</div>	
 	</div>
